42d618d3aa7b8e6b1a00176bcf72edcef4092628
Grant Shively <gshively@godaddy.com>
2020-12-03 13:01:11 -0700

    topdown/http was not properly closing responses, resulting in a memory leak

--- topdown/http.go
+++ topdown/http.go
@@ -23,6 +23,7 @@ import (
 	"github.com/open-policy-agent/opa/ast"
 	"github.com/open-policy-agent/opa/internal/version"
 	"github.com/open-policy-agent/opa/topdown/builtins"
+	"github.com/open-policy-agent/opa/util"
 )
 
 const defaultHTTPRequestTimeoutEnv = "HTTP_SEND_TIMEOUT"
@@ -133,7 +134,7 @@ func getHTTPResponse(bctx BuiltinContext, req ast.Object) (*ast.Term, error) {
 		if err != nil {
 			return nil, err
 		}
-
+		defer util.Close(httpResp)
 		// add result to cache
 		resp, err = reqExecutor.InsertIntoCache(httpResp)
 		if err != nil {
@@ -583,7 +584,7 @@ func checkHTTPSendInterQueryCache(bctx BuiltinContext, key ast.Object, req *http
 		return nil, err
 	}
 
-	defer result.Body.Close()
+	defer util.Close(result)
 
 	var newValue ast.Value
 	var size int
